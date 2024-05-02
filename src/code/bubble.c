dbb78d468f52352ab1883d84b677169f8dcecf54
EdwinOrtegaGutierrez <eddyded49@gmail.com>
2024-05-02 05:53:45 -0600

    Feat: Bubble

--- src/code/bubble.c
+++ src/code/bubble.c
@@ -1,26 +1,39 @@
-#include<stdio.h>
+// Bubble sort in C
+#include <stdio.h>
 
-void bubbleSort(int arr[], int n) {
+// perform the bubble sort
+void bubbleSort(int array[], int size) {
+    int newSize = size - 1;
+    // loop to access each array element
+    for (int step = 0; step < newSize; ++step) {
+        // loop to compare array elements
+        for (int i = 0; i < size - step - 1; ++i) {    
+            // compare two adjacent elements
+            // change > to < to sort in descending order
+            if (array[i] > array[i + 1]) {
+                // swapping occurs if elements
+                // are not in the intended order
+                int temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+            }
+        }
+    }
+}
 
-    int newN = n - 1;
+// print array
+void printArray(int array[], int size) {
+    for (int i = 0; i < size; ++i) {
+        printf("%d  ", array[i]);
+    }
+    printf("\n");
+}
 
-    for (int i = 0; i < newN; i++) {  
-        for (int j = 0; j < n - i - 1; j++) {  
-            if (arr[j] > arr[j + 1]) {  
-                int temp = arr[j];  
-                arr[j] = arr[j + 1];  
-                arr[j + 1] = temp;  
-            }  
-        }  
-    }  
-}  
-int main() {  
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};  
-    int n = sizeof(arr) / sizeof(arr[0]);  
-    bubbleSort(arr, n);  
-    printf("Sorted array: ");  
-    for (int i = 0; i < n; i++) {  
-        printf("%d ", arr[i]);  
-    }  
-    return 0;  
+int main() {
+    int data[] = {-2, 45, 0, 11, -9};  
+    // find the array's length
+    int size = sizeof(data) / sizeof(data[0]);
+    bubbleSort(data, size);
+    printf("Sorted Array in Ascending Order:\n");
+    printArray(data, size);
 }
